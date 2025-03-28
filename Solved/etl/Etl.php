2c37a5e9bf38e8499bce7a92349bb353e4bc247a
Julian <jacaminaur@gmail.com>
2025-03-28 14:55:35 +0100

    ETL solved with walk array

--- Solved/etl/Etl.php
+++ Solved/etl/Etl.php
@@ -6,12 +6,19 @@ function transform(array $oldValues): array
 {
     $newValues = [];
 
-    foreach ($oldValues as $key => $array) {
-        foreach ($array as $letter) {
-            $newValues[strtolower($letter)] = $key;
-        }
-    }
+    // Solution 1
+    // foreach ($oldValues as $key => $array) {
+    //     foreach ($array as $letter) {
+    //         $newValues[strtolower($letter)] = $key;
+    //     }
+    // }
 
-   
+    // Solucion 2 
+    array_walk($oldValues, function($array,$key) use(&$newValues) {
+        array_walk($array, function($letter) use($key,&$newValues){
+            $newValues[strtolower($letter)] = $key;
+        });
+    });
+
     return $newValues;
 }
