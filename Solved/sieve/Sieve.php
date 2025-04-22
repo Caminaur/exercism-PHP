4e2c713a3caceec9d89189225737537edfbddab9
Julian <jacaminaur@gmail.com>
2025-04-22 10:28:31 +0200

    Sieve Solution

---
+++ Solved/sieve/Sieve.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Devuelve un array con todos los números primos hasta un límite dado
+ *
+ * @param int $number El número máximo a evaluar
+ * @return int[] Lista de números primos
+ */
+function sieve(int $number): array
+{
+    $arr_numbers = range(1, $number);
+    $values_to_remove = [1];
+
+    foreach ($arr_numbers as $n) {
+        if (in_array($n, $values_to_remove)) continue;
+
+        $multiplier = 2; // 1 2 3 4 ...
+        while (($n * $multiplier) <= $number) { // 4 6 8 10 12 14
+            $values_to_remove[] = $n * $multiplier;
+            $multiplier++;
+        }
+    }
+
+    $prime_numbers = array_filter($arr_numbers, function ($number) use ($values_to_remove) {
+        return !in_array($number, $values_to_remove);
+    });
+    return array_values($prime_numbers);
+}
+
+// array con numero hasta N
+// por cada numero === n
+    // set
+    // n*i < $number?
+        // set values_to_remove
