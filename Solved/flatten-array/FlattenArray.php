e266a9439f693620b2629b481757649c2e726dba
Julian <jacaminaur@gmail.com>
2025-05-10 08:10:41 +0200

    Flatten Array shorter

--- Solved/flatten-array/FlattenArray.php
+++ Solved/flatten-array/FlattenArray.php
@@ -2,23 +2,6 @@
 
 declare(strict_types=1);
 
-
-function ciclar_array($array): array
-{
-    $response = [];
-
-    foreach ($array as $v) {
-        if ($v === null) continue;
-        if (is_array($v)) {
-            $response = array_merge($response, ciclar_array($v));
-            continue;
-        }
-        $response[] = $v;
-    }
-
-    return $response;
-}
-
 function flatten(array $input): array
 {
     $response = [];
@@ -26,16 +9,11 @@ function flatten(array $input): array
         if ($item === null) continue;
 
         if (is_array($item)) {
-            $response = array_merge($response, ciclar_array($item));
-            continue;
+            $response = array_merge($response, flatten($item));
+        } else {
+            $response[] = $item;
         }
-        $response[] = $item;
     }
 
     return $response;
 }
-
-# Crear el Response Array
-# Tomar la lista y ciclarla
-# Si el objeto es un array, iterar cada objeto del mismo y agregarlo al Response array
-# Tener cuidado con que tan nested esta el array
