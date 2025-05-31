a52a90dfbfbf373bef48c6f5bf5cafc074c4197a
Julian <jacaminaur@gmail.com>
2025-05-31 06:30:41 +0200

    Grains Solution

--- Solved/grains/Grains.php
+++ Solved/grains/Grains.php
@@ -1,19 +1,80 @@
 <?php
 
+// Solucion sin bc library
+
 declare(strict_types=1);
 
-function square(int $number): string
+function square(int $n): string
 {
-    if ($number <= 0 || $number > 64) {
-        throw new InvalidArgumentException("The number must be between 1 and 64");
+    if ($n <= 0 || $n > 64) {
+        throw new InvalidArgumentException("Debe estar entre 1 y 64");
     }
-    return bcpow('2', (string)($number - 1));
+
+    return powerOfTwo($n - 1);
 }
 
 function total(): string
 {
-    return bcsub(bcpow('2', '64'), '1');
+    return subtractStrings(powerOfTwo(64), '1');
 }
+
+function powerOfTwo(int $exp): string
+{
+    $result = '1';
+
+    for ($i = 0; $i < $exp; $i++) {
+        $result = multiplyByTwo($result);
+    }
+
+    return $result;
+}
+
+function multiplyByTwo(string $num): string
+{
+    $carry = 0;
+    $res = '';
+
+    for ($i = strlen($num) - 1; $i >= 0; $i--) {
+        $digit = (int)$num[$i] * 2 + $carry;
+        $res = ($digit % 10) . $res;
+        $carry = intdiv($digit, 10);
+    }
+
+    if ($carry > 0) {
+        $res = $carry . $res;
+    }
+
+    return $res;
+}
+
+function subtractStrings(string $mayor, string $menor): string
+{
+    $longitud = max(strlen($mayor), strlen($menor));
+    $mayor = str_pad($mayor, $longitud, '0', STR_PAD_LEFT);
+    $menor = str_pad($menor, $longitud, '0', STR_PAD_LEFT);
+
+    $resultado = '';
+    $prestamo = 0;
+
+    for ($i = $longitud - 1; $i >= 0; $i--) {
+        $digMayor = (int)$mayor[$i];
+        $digMenor = (int)$menor[$i];
+
+        $digito = $digMayor - $digMenor - $prestamo;
+
+        if ($digito < 0) {
+            $digito += 10;
+            $prestamo = 1;
+        } else {
+            $prestamo = 0;
+        }
+
+        $resultado = $digito . $resultado;
+    }
+
+    return ltrim($resultado, '0') ?: '0';
+}
+
 
 // https://www.php.net/manual/en/function.bcpow.php
 // https://www.php.net/manual/en/function.bcsub.php
@@ -26,7 +87,7 @@ function total(): string
 // 1 2 4 8 16 32 64 128
 // 1 2 4 8 16 32 64 128
 // 1 2 4 8 16 32 64 128
-// 1 2 4 8 16 32 64 128
+// 1 2 4 8 16 32 64 128 
 // 1 2 4 8 16 32 64 128
 // 1 2 4 8 16 32 64 128
 // 1 2 4 8 16 32 64 128
