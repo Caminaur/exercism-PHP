5004a33679351321a24fc7a4ad0b7abb9148dd19
Julian <jacaminaur@gmail.com>
2025-08-25 08:57:56 +0200

    Darts Solution 2

--- Solved/darts/Darts.php
+++ Solved/darts/Darts.php
@@ -4,14 +4,9 @@ declare(strict_types=1);
 
 function score(float $leg1, float $leg2): int
 {
-    $hypotenuse = sqrt((pow($leg1, 2) + pow($leg2, 2)));
-    if ($hypotenuse <= 1) {
-        return 10;
-    } elseif ($hypotenuse >= 1 && $hypotenuse <= 5) {
-        return 5;
-    } elseif ($hypotenuse >= 5 && $hypotenuse <= 10) {
-        return 1;
-    } else {
-        return 0;
-    }
+    $hypotenuse = hypot($leg1, $leg2);
+    if ($hypotenuse <= 1) return 10;
+    if ($hypotenuse <= 5) return 5;
+    if ($hypotenuse <= 10) return 1;
+    return 0;
 }
