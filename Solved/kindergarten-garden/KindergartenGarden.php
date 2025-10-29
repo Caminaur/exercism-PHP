fd8ee1bebacd06a7386c1eb406218298422d4f71
Julian <jacaminaur@gmail.com>
2025-10-29 12:04:53 +0100

    Refactor KindergartenGarden for clarity and type safety

--- Solved/kindergarten-garden/KindergartenGarden.php
+++ Solved/kindergarten-garden/KindergartenGarden.php
@@ -1,40 +1,48 @@
 <?php
 
 declare(strict_types=1);
-
 class KindergartenGarden
 {
-    private $plants_DICT = [
-        "R" => 'radishes',
-        "C" => 'clover',
-        "G" => 'grass',
-        "V" => 'violets'
+    private const PLANT_MAP = ["R" => 'radishes', "C" => 'clover', "G" => 'grass', "V" => 'violets'];
+    private const STUDENTEN = [
+        'Alice',
+        'Bob',
+        'Charlie',
+        'David',
+        'Eve',
+        'Fred',
+        'Ginny',
+        'Harriet',
+        'Ileana',
+        'Joseph',
+        'Kincaid',
+        'Larry'
     ];
-    private $studenten = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry'];
     private $line_1 = [];
     private $line_2 = [];
     public function __construct(string $diagram)
     {
-        $lines = explode("\n", $diagram);
-        $this->line_1 = $lines[0];
-        $this->line_2 = $lines[1];
+        [$this->line_1, $this->line_2] = explode("\n", $diagram);
     }
-
     private function codeToName(string $plant): string
     {
-        return  $this->plants_DICT[$plant];
+        return self::PLANT_MAP[$plant] ?? "unknown";
     }
     public function plants(string $student): array
     {
-        $key = array_search($student, $this->studenten);
-        $k = $key * 2;
+        $key = array_search($student, self::STUDENTEN);
+
+        $plant1Key = $key * 2;
+        $plant2Key = $plant1Key + 1;
+
         $line_1 = $this->line_1;
         $line_2 = $this->line_2;
+
         $flowers = [];
-        $flowers[] =  $this->codeToName($line_1[$k]);
-        $flowers[] =  $this->codeToName($line_1[$k + 1]);
-        $flowers[] =  $this->codeToName($line_2[$k]);
-        $flowers[] =  $this->codeToName($line_2[$k + 1]);
+        $flowers[] = $this->codeToName($line_1[$plant1Key]);
+        $flowers[] = $this->codeToName($line_1[$plant2Key]);
+        $flowers[] = $this->codeToName($line_2[$plant1Key]);
+        $flowers[] = $this->codeToName($line_2[$plant2Key]);
         return $flowers;
     }
 }
